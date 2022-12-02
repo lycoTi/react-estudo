07d2346727f5a08246eac614d36c92d9bd9cc350
Tiago <tiagosantos__@live.com>
2022-12-02 13:06:10 -0400

    adicionando rotas

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -1,7 +1,9 @@
+import { Link } from 'react-router-dom'
+
 const Home = () => {
     return (<>
       <h1>Home</h1>
-      <button>Fazer Login</button>
+      <Link to="/login">Fazer Login</Link>
     </>) 
 }
 export { Home };
--- src/pages/login/index.jsx
+++ src/pages/login/index.jsx
@@ -1,7 +1,8 @@
+import { Link } from "react-router-dom"
 const Login = () => {
   return (<>
     <h1>Login</h1>
-    <button>Voltar para home</button>
+    <Link to="/">Voltar para home</Link>
     </>)
 }
 
